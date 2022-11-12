3a3fe060a6ae438225e504a96fa2e0847ede6842
expiredy101 <yaraoley2005@mail.ru>
2022-11-12 14:37:49 +0300

    Build9ng structrue

---
+++ main.py
@@ -0,0 +1,10 @@
+from api.telegram_client import TelegramClient
+from database.database_session import DatabaseSession
+
+
+def main():
+    telegram_client = TelegramClient()
+
+
+if __name__ == "__main__":
+    main()
